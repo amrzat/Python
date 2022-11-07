fa5534f00157aef9b99fbffc01f12d3ff98488da
Amer Izzat <114406689+amrzat@users.noreply.github.com>
2022-11-07 19:45:29 +0800

    Greeting world and your age for next year

---
+++ greeting.py
@@ -0,0 +1,7 @@
+print("What is your name?")
+name = input()
+print("What is your age?")
+age = input()
+age_next_year = int(age) + 1
+print("Hi " + name)
+print("You will be " + str(age_next_year) + " years old next year.")
